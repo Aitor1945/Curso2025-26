9c01bc574c11c8970b93eba1a0a5f18dd3ec2c15
Aitor <agj0034@alu.medac.es>
2025-10-20 19:13:58 +0200

    holacommit

--- DES/Tema1/Ejercicios/Ej1Bootstrap.php
+++ DES/Tema1/Ejercicios/Ej1Bootstrap.php
@@ -6,8 +6,94 @@
     <title>Ejercicio 1 Bootstrap</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
+
 <body>
- <p>Hola Mundo</p>
+
+ Estoy fuera de un contenedor de Bootstrap.
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+<div class="container bg-success">
+    Esto en un contenedor de Bootstrap.
+</div>
+
+<div class="container-fluid bg-primary">
+    Esto en un contenedor fluido de Bootstrap.
+</div>
+
+
+<div class="container mt-5">
+ <div class="row">
+    <div class="col bg-danger"> 1 </div>
+    <div class="col bg-primary"> 2 </div>
+    <div class="col bg-success"> 3 </div>
+    <div class="col bg-danger"> 4 </div>
+    <div class="col bg-warning"> 5 </div>
+    <div class="col bg-secondary"> 6 </div>
+    <div class="col bg-danger"> 7 </div>
+    <div class="col bg-secondary"> 8 </div>
+    <div class="col bg-success"> 9 </div>
+    <div class="col bg-warning"> 10 </div>
+    <div class="col bg-primary"> 11 </div>
+    <div class="col bg-secondary"> 12 </div>
+  </div>
+</div>
+
+<div class="container mt-5">
+ <div class="row">
+    <div class="col-12 col-xl-3 bg-primary"> 
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        It has survived not only five centuries, but also the leap into electronic typesetting,
+        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </div>
+   <div class="col-12 col-xl-3 bg-success"> 
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        It has survived not only five centuries, but also the leap into electronic typesetting,
+        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </div>
+       <div class="col-12 col-xl-3 bg-danger"> 
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        It has survived not only five centuries, but also the leap into electronic typesetting,
+        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </div>
+       <div class="col-12 col-xl-3 bg-secondary"> 
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        It has survived not only five centuries, but also the leap into electronic typesetting,
+        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </div>
+  </div>
+</div>
+
+<div class="container mt-5">
+ <div class="row">
+    <div class="col-1"> 
+
+    </div>
+    <div class="col-1"> 
+        
+    </div>
+  </div>
+</div>
+
+
+<div class="container mt-5">
+ <div class="row">
+    <div class="col-3 bg-danger"> 3 </div>
+    <div class="col-6 bg-primary"> 9 </div>
+    <div class="col-3 bg-success"> 12 </div>
+  </div>
+</div>
+
 </body>
 </html>

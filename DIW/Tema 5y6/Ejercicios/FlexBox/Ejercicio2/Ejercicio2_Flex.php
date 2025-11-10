9e5d4fb1e4d9e71f253c209c2683edf2a90f94c6
Aitor <agj0034@alu.medac.es>
2025-11-10 21:28:14 +0100

    Main

---
+++ DIW/Tema 5y6/Ejercicios/FlexBox/Ejercicio2/Ejercicio2_Flex.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flex Box - DIW</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section class="parent">
+    <div class="container">
+    CSS IS AWESOME:Visible
+    </div>
+    </section>
+    <section class="parent">
+    <div class="container">
+    CSS IS AWESOME:Visible
+    </div>
+    </section>
+    <section class="parent">
+    <div class="container">
+    CSS IS AWESOME:Visible
+    </div>
+    </section>   
+
+
+    <section class="parent">
+    <div class="container2">
+    CSS IS AWESOME:Visible
+    </div>
+    </section>
+    <section class="parent">
+    <div class="container2">
+    CSS IS AWESOME:Visible
+    </div>
+    </section>
+    <section class="parent">
+    <div class="container2">
+    CSS IS AWESOME:Visible
+    </div>
+    </section>   
+
+        <section class="parent1">
+    <div class="container3">
+    CSS IS AWESOME:Visible
+    </div>
+    </section>
+    <section class="parent1">
+    <div class="container3">
+    CSS IS AWESOME:Visible
+    </div>
+    </section>
+    <section class="parent1">
+    <div class="container3">
+    CSS IS AWESOME:Visible
+    </div>
+    </section> 
+
+</body>
+</html>
---
+++ DIW/Tema 5y6/Ejercicios/FlexBox/Ejercicio3/Ejercicio3_Flex.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flex Box - DIW</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <div class="container">
+    <div class="first">Arriba</div>
+    <div class="second">Izquierda</div>
+    <div class="last">Derecha</div>
+  </div>
+</body>
+
+</html>
